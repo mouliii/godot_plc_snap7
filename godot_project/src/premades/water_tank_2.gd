6ea0a142bc7dec6155f2006757d26b49a01e7a0d
Aleksi Räsänen <rasanen.aleksi@hotmail.com>
2026-02-06 18:19:45 +0200

    tank fixes

--- godot_project/src/premades/water_tank_2.gd
+++ godot_project/src/premades/water_tank_2.gd
@@ -2,16 +2,18 @@ extends StaticBody3D
 
 @onready var inTag: PLCTag = $TankFillIn/PLCTag
 @onready var outTag: PLCTag = $TankEmptyOut/PLCTag
+@onready var FillLevelTag: PLCTag = $TankFillLevel/PLCTag
 @onready var fillLevelTag: PLCTag = $TankFillLevel/PLCTag
-@onready var plcTimer: Timer = $TankFillLevel/UpdatePLCTimer
+@onready var plcTimer: Timer = $UpdatePLCTimer
 @onready var innerWall: MeshInstance3D = $water_tank/InnerWall
-
+## in leters
 @export var maxFillRate := 50.0
+## in liters
+@export var maxVolume := 1500.0
 @export var plcUpdateInterval := 0.2
 
 var curVolume := 0.0
 var curFillRate := 0.0
-const maxVolume := 1500.0
 
 func _ready() -> void:
 	$InflowParticles.emitting = false
@@ -19,31 +21,28 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	var inFlow:float = remap(float(inTag.Read()), 0.0, Snap7Cpp.analogValueMax, 0, maxFillRate)
-	var outFlow:float = remap(float(outTag.Read()), 0.0, Snap7Cpp.analogValueMax, 0, maxFillRate)
+	var inFlow:float = remap(inTag.Read(), 0.0, Snap7Cpp.analogValueMax, 0, maxFillRate)
+	var outFlow:float = remap(outTag.Read(), 0.0, Snap7Cpp.analogValueMax, 0, maxFillRate)
 	curFillRate = inFlow - outFlow
-
-	var fillrateInLiters = remap(curFillRate, 0.0, Snap7Cpp.analogValueMax, 0, maxFillRate)
-	curVolume += fillrateInLiters * delta
-	curVolume = clampf(curVolume, 0, maxVolume)
-	var levelNormalized:float = curVolume / maxVolume
-
+	curVolume = clampf(curVolume + curFillRate * delta, 0, maxVolume)
+	
 	if !is_zero_approx(curFillRate):
 		if plcTimer.is_stopped():
 			plcTimer.start(plcUpdateInterval)
+			WriteToPlc()
 	else:
 		plcTimer.stop()
 
+	var levelNormalized:float = curVolume / maxVolume
 	innerWall.get_active_material(0).set_shader_parameter("waterLevel", levelNormalized)
-	if inFlow > 0.0001:
+	if !is_zero_approx(inFlow):
 		SetInflowParticles(true)
 	else:
 		SetInflowParticles(false)
-	if outFlow > 0.0001:
+	if !is_zero_approx(outFlow):
 		SetOutflowParticles(true)
 	else:
 		SetOutflowParticles(false)
-
 
 func SetInflowParticles(enabled:bool)->void:
 	$InflowParticles.emitting = enabled
@@ -51,6 +50,9 @@ func SetInflowParticles(enabled:bool)->void:
 func SetOutflowParticles(enabled:bool)->void:
 	$OutfloParticles.emitting = enabled
 
-
 func _on_update_plc_timer_timeout() -> void:
-	pass # Replace with function body.
+	WriteToPlc()
+
+func WriteToPlc()->void:
+	var level := remap(curVolume, 0.0, maxVolume, 0, Snap7Cpp.analogValueMax)
+	FillLevelTag.Write(int(level))
